fc0404dff1226beabfe9908b3fe42bc55c7d81bf
Andrew Davidoff <andreida@microsoft.com>
2016-04-18 11:00:02 +0800

    Small fix for Start/EndDate format

--- UWP/Meeting_Manager_UWP/MeetingManager/HttpGraphService.cs
+++ UWP/Meeting_Manager_UWP/MeetingManager/HttpGraphService.cs
@@ -41,8 +41,8 @@ namespace MeetingManager
 
         private string BuildCalendarUri(DateTimeOffset startDate, DateTimeOffset endDate)
         {
-            string start = startDate.ToString("O");
-            string end = endDate.ToString("O");
+            string start = startDate.Date.ToString("s");
+            string end = endDate.Date.ToString("s");
 
             string uri = $"calendarview?startDateTime={start}&endDateTime={end}";
             uri += "&$orderby=start/dateTime";
